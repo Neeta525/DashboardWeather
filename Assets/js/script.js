6e3ed23cd0264d08c9a22d13556543ea06fbe915
Anita Chavez <anita.chavez@carlsbadschools.net>
2022-04-05 22:53:19 -0600

    refactoring code, added local stroage js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -15,11 +15,7 @@ var cityButton = document.querySelector("#cityButton");
 var forecastContainerEl = document.querySelector("#forecast-container");
 var forecast5 = document.querySelector("#forecast5");
 var forecastList = document.querySelector("#forecastList");
-// var day1 = document.querySelector("#card1");
-// var day2 = document.querySelector("#card2");
-// var day3 = document.querySelector("#card3");
-// var day4 = document.querySelector("#card4");
-// var day5 = document.querySelector("#card5");
+
 var today = moment().format("MMMM Do, YYYY");
 
 //API key from open weather map
@@ -36,67 +32,67 @@ var formSubmitHandler = function (event) {
         currentEl.textContent = searchCity + " " + today;
     }
 };
-//Local Storage-**not working right now
-// var citiesList = [];
-// //function for history-local storage
 
-// function renderLastCity() {
-//     cityList.innerHTML = "";
-//     historyContainerSpan.textContent = citiesList.length;
+var citiesList = [];
 
-//     for (var i = 0; i < citiesList.length; i++) {
-//         var list = citiesList[i];
+// function for searched cities-local storage
+function renderLastCity() {
+    cityList.innerHTML = "";
 
-//         var li = document.createElement("li");
-//         li.textContent = list;
-//         li.setAttribute("data-index", i);
+    for (var i = 0; i < citiesList.length; i++) {
+        var list = citiesList[i];
 
-//         var button = document.createElement("button");
-//         button.textContent = ("searchCity");
+        var li = document.createElement("li");
+        li.textContent = list;
+        li.setAttribute("data-index", i);
 
-//         li.appendChild(button);
-//         citiesList.appendChild(li);
-//     }
-// }
+        var button = document.createElement("button");
+        button.textContent = ("Search it Again");
 
-// function init() {
-//     var storedCities = JSON.parse(localStorage.getItem("citiesList"));
-//     if (storedCities !== null) {
-//         citiesList = storedCities;
-//     }
-//     renderLastCity();
-// }
+        li.appendChild(button);
+        cityList.appendChild(li);
+    }
+}
 
-// function storeCities() {
-//     localStorage.setItem("citiesList", JSON.stringify(citiesList));
-// }
-// searchFormEl.addEventListener("submit", function(event) {
-//     event.preventDefault();
-//     var listText = cityInputEl.value.trim();
+function init() {
+    var storedCities = JSON.parse(localStorage.getItem("citiesList"));
+    if (storedCities !== null) {
+        citiesList = storedCities;
+    }
+    renderLastCity();
+}
 
-//     if (listText === "") {
-//         return;
-//     }
-//     citiesList.push(listText);
-//     cityInputEl.value = "";
+function storeCities() {
+    localStorage.setItem("citiesList", JSON.stringify(citiesList));
+}
+searchFormEl.addEventListener("submit", function(event) {
+    event.preventDefault();
+    var listText = cityInputEl.value.trim();
 
-//     storeCities();
-//     renderLastCity();
-// });
+    if (listText === "") {
+        return;
+    }
+    citiesList.push(listText);
+    cityInputEl.value = "";
 
-// citiesList.addEventListener("click", function(event) {
+    storeCities();
+    renderLastCity();
+});
+
+// Add click event to list of cities searched
+// todoList.addEventListener("click", function(event) {
 //     var element = event.target;
 
 //     if (element.matches("button") === true) {
-//         var index = element.parentElement.getAttribute("data-index");
-//         citiesList.splice(index, 1);
+//         var index = 
 
-//         storeCities();
-//         renderLastCity();
+//     storeTodos();
+//     renderTodos();
 //     }
-// })
+// });
 
-// init();
+init();
+
 
 //Get data from the URL
 var getCityWeather = function (lat, lon) {
@@ -114,9 +110,6 @@ var getCityWeather = function (lat, lon) {
             console.log(data);
             displayCity(data);
             displayInfo(data);
-            
-            // getDetails();
-            // getUV();
             });
         }
     })
@@ -131,10 +124,10 @@ var getCityWeather = function (lat, lon) {
 function forecastDisplay(data) {
     for (var i = 0; i < 5; i++) {
         var container = document.createElement("div");
-        var date = moment().format('L'); 
+        var date = "the day after"; 
+        container.classList.add("card2");
         container.classList.add("bg-dark");
         container.classList.add("text-white");
-        container.classList.add("card");
         var li = document.createElement("li");
         var cardTitle = document.createElement("h5");
         cardTitle.textContent = date;
@@ -155,13 +148,12 @@ function forecastDisplay(data) {
 }
 
 
-
 //Function for longitude and latitude 
 function forecast(lat, long) {
     var getForecast = "https://api.openweathermap.org/data/2.5/forecast?";
     var getlat = "lat=" + lat;
     var getlong = "&lon=" + long;
-    var rest = "&cnt=5&appid=";
+    var rest = "&cnt=5&units=imperial&appid=";
     
     fetch(getForecast + getlat + getlong + rest + APIKey)
     .then(function(response){
@@ -175,7 +167,7 @@ function forecast(lat, long) {
     })
 }
 
-//This function 
+//This function calls the city name and location
 function getCoords() {
     var getCoord = "http://api.openweathermap.org/geo/1.0/direct?q=";
     var cityName = cityInputEl.value;
@@ -198,7 +190,7 @@ function getCoords() {
 var displayCity = function (cities) {
 
 for (var i = 0; i < cities.length; i++) {
-    var current = document.createElement("h4")
+    var current = document.createElement("h2")
     var tempCurrent = document.createElement("p");
     var windCurrent = document.createElement("p");
     var humidityCurrent = document.createElement("p");
@@ -216,7 +208,6 @@ for (var i = 0; i < cities.length; i++) {
 }
 }
 
-
 function displayInfo(data) {
     
     currentTempEl.textContent = "Temp: " + data.current.temp + "\u00B0" + "F";
@@ -224,7 +215,6 @@ function displayInfo(data) {
     currentHumidityEl.textContent = "Humidity: " + data.current.humidity + "%";
     currentIndexEl.textContent = "UV Index: " + data.current.uvi;
 }
-
 
 
 searchFormEl.addEventListener('click', formSubmitHandler);
